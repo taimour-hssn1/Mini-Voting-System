be1cb43a6f3ec499db4f7f299ae57fdd26388c6b
taimour-hssn1 <bcsf22m504@pucit.edu.pk>
2025-06-27 21:08:07 -0700

    Errors Resolved

--- MAIN.C
+++ MAIN.C
@@ -2,7 +2,7 @@
 
 int main(){
     while(1){
-        printf("\n\t\t\t   1.Student panel \n\t\t\t   2.Admin panel \n\t\t\t   3.Exit \n\t\t\t   Option:");
+        printf("\n\t\t\t   1.Student panel \n\t\t\t   2.Admin panel \n\t\t\t   3.Quit \n\t\t\t   Options:");
 		char input;
         scanf(" %c",&input);
 
